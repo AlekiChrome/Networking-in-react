0b34d10391feeb47c18aef2b771c78d3512ee113
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-05-02 23:25:52 -0400

    initial commit

---
+++ src/NumberOfDogs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function NumberOfDogs(props) {
+
+    return (
+        <div>
+            <input type="number" onChange={props.handleChange} value={props.dogCount} />
+        </div>
+    )
+}
+
+
